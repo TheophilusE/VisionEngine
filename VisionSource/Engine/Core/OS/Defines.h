16e46d14aae0fa87721d90c39596a888d782d152
THEO <65521326+GodTHEO@users.noreply.github.com>
2021-08-24 08:48:03 -0600

    Add Json Types And Defines

--- VisionSource/Engine/Core/OS/Defines.h
+++ VisionSource/Engine/Core/OS/Defines.h
@@ -116,3 +116,144 @@ P Extract(const Q& v)
 {
 	return std::get<P>(v);
 }
+
+#include <nlohmann/json.hpp>
+/// Namespace for the JSON library
+namespace JSON = nlohmann;
+
+namespace nlohmann
+{
+template <>
+struct adl_serializer<Diligent::float2>
+{
+	static void to_json(json& j, const Diligent::float2& v)
+	{
+		j["x"] = v.x;
+		j["y"] = v.y;
+	}
+
+	static void from_json(const json& j, Diligent::float2& v)
+	{
+		v.x = j.at("x");
+		v.y = j.at("y");
+	}
+};
+template <>
+struct adl_serializer<Diligent::float3>
+{
+	static void to_json(json& j, const Diligent::float3& v)
+	{
+		j["x"] = v.x;
+		j["y"] = v.y;
+		j["z"] = v.z;
+	}
+
+	static void from_json(const json& j, Diligent::float3& v)
+	{
+		v.x = j.at("x");
+		v.y = j.at("y");
+		v.z = j.at("z");
+	}
+};
+template <>
+struct adl_serializer<Diligent::float4>
+{
+	static void to_json(json& j, const Diligent::float4& v)
+	{
+		j["x"] = v.x;
+		j["y"] = v.y;
+		j["z"] = v.z;
+		j["w"] = v.w;
+	}
+
+	static void from_json(const json& j, Diligent::float4& v)
+	{
+		v.x = j.at("x");
+		v.y = j.at("y");
+		v.z = j.at("z");
+		v.w = j.at("w");
+	}
+};
+/*
+template <>
+struct adl_serializer<Color>
+{
+	static void to_json(json& j, const Color& v)
+	{
+		j["r"] = v.R();
+		j["g"] = v.G();
+		j["b"] = v.B();
+		j["a"] = v.A();
+	}
+
+	static void from_json(const json& j, Color& v)
+	{
+		v.x = j.at("r");
+		v.y = j.at("g");
+		v.z = j.at("b");
+		v.w = j.at("a");
+	}
+};
+*/
+template <>
+struct adl_serializer<Diligent::Quaternion>
+{
+	static void to_json(json& j, const Diligent::Quaternion& v)
+	{
+		j["x"] = v.q.x;
+		j["y"] = v.q.y;
+		j["z"] = v.q.z;
+		j["w"] = v.q.w;
+	}
+
+	static void from_json(const json& j, Diligent::Quaternion& v)
+	{
+		v.q.x = j.value("x", 0.0f);
+		v.q.y = j.value("y", 0.0f);
+		v.q.z = j.value("z", 0.0f);
+		v.q.w = j.value("w", 0.0f);
+	}
+};
+template <>
+struct adl_serializer<Diligent::float4x4>
+{
+	static void to_json(json& j, const Diligent::float4x4& v)
+	{
+		for (int x = 0; x < 4; x++)
+		{
+			for (int y = 0; y < 4; y++)
+			{
+				j.push_back(v.m[x][y]);
+			}
+		}
+	}
+
+	static void from_json(const json& j, Diligent::float4x4& v)
+	{
+		for (int x = 0; x < 4; x++)
+		{
+			for (int y = 0; y < 4; y++)
+			{
+				v.m[x][y] = j[x * 4 + y];
+			}
+		}
+	}
+};
+/*
+template <>
+struct adl_serializer<BoundingBox>
+{
+	static void to_json(json& j, const BoundingBox& v)
+	{
+		j["center"] = (float3)v.Center;
+		j["extents"] = (float3)v.Extents;
+	}
+
+	static void from_json(const json& j, BoundingBox& v)
+	{
+		v.Center = (float3)j.value("center", Vector3::Zero);
+		v.Extents = (float3)j.value("extents", Vector3 { 0.5f, 0.5f, 0.5f });
+	}
+};
+*/
+}
