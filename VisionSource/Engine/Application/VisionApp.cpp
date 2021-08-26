c1715a34815dafb352d8bad85f8467f1618fc584
THEO <65521326+GodTHEO@users.noreply.github.com>
2021-08-25 18:24:14 -0600

    Update UI

--- VisionSource/Engine/Application/VisionApp.cpp
+++ VisionSource/Engine/Application/VisionApp.cpp
@@ -66,6 +66,7 @@ void VisionApp::UpdateUI()
         auto& light = m_DirectionalLight.GetComponent<DirectionalLightComponent>();
         auto& m     = m_Helmet.GetComponent<MeshComponent>();
         auto& t     = m_Helmet.GetComponent<TransformComponent>();
+        auto& c     = m_Camera.GetComponent<CameraComponent>();
 
         ImGui::SetNextWindowPos(ImVec2(10, 10), ImGuiCond_FirstUseEver);
         if (ImGui::Begin("Settings", nullptr, ImGuiWindowFlags_AlwaysAutoResize))
@@ -87,6 +88,46 @@ void VisionApp::UpdateUI()
                 ImGui::gizmo3D("Model Rotation", t.Rotation, ImGui::GetTextLineHeight() * 10);
                 ImGui::SameLine();
                 ImGui::gizmo3D("Light direction", light.m_LightDirection, ImGui::GetTextLineHeight() * 10);
+            }
+
+            {
+                if (ImGui::TreeNode("Lighting"))
+                {
+                    ImGui::ColorEdit3("Light Color", &light.m_LightColor.r);
+                    // clang-format off
+                    ImGui::SliderFloat("Light Intensity", &light.m_LightIntensity, 0.f, 100.f);
+                    //ImGui::SliderFloat("Occlusion strength", &m_RenderParams.OcclusionStrength, 0.f,  1.f);
+                    //ImGui::SliderFloat("Emission scale",     &m_RenderParams.EmissionScale,     0.f,  1.f);
+                    //ImGui::SliderFloat("IBL scale",          &m_RenderParams.IBLScale,          0.f,  1.f);
+                    // clang-format on
+                    ImGui::TreePop();
+                }
+            }
+
+            {
+                if (ImGui::Button("Reset view"))
+                {
+                    c.m_Camera.SetPos(float3{});
+                    c.m_Camera.SetRotation(0.f, 0.f);
+                    t.Translation = float3{};
+                    t.Rotation    = Quaternion::RotationFromAxisAngle(float3{0.f, 1.0f, 0.0f}, -PI_F / 2.f);
+                }
+            }
+
+            {
+                if (!m.m_Model->Animations.empty())
+                {
+                    ImGui::SetNextTreeNodeOpen(true, ImGuiCond_FirstUseEver);
+                    if (ImGui::TreeNode("Model Animation"))
+                    {
+                        ImGui::Checkbox("Play Animation", &m.m_PlayAnimation);
+                        Vector<const char*> Animations(m.m_Model->Animations.size());
+                        for (size_t i = 0; i < m.m_Model->Animations.size(); ++i)
+                            Animations[i] = m.m_Model->Animations[i].Name.c_str();
+                        ImGui::Combo("Current Animation", reinterpret_cast<int*>(&m.m_AnimationIndex), Animations.data(), static_cast<int>(Animations.size()));
+                        ImGui::TreePop();
+                    }
+                }
             }
         }
         ImGui::End();
